9f7b82d424735820a395cfedafa3ef9ef211b991
Bichen Wu <bichen@berkeley.edu>
2016-12-09 17:27:23 -0800

    bichen: add demo script and one example image.
    On branch master
    Your branch is up-to-date with 'origin/master'.
    
    Changes to be committed:
    modified:   data/sample.png
    new file:   src/demo.py

---
+++ src/demo.py
@@ -0,0 +1,208 @@
+# Author: Bichen Wu (bichen@berkeley.edu) 08/25/2016
+
+"""SqueezeDet Demo. 
+
+In image detection mode, for a given image, detect object and draw bounding
+boxes around it. In video detection mode, perform real-time detection on the
+video stream.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+import time
+import sys
+import os
+import glob 
+
+import numpy as np
+import tensorflow as tf
+
+from config import *
+from train import _draw_box
+from nets import *
+
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string(
+    'mode', 'image', """'image' or 'video'.""")
+tf.app.flags.DEFINE_string(
+    'checkpoint_dir', './data/model_checkpoints/squeezeDet/', """Path to the training checkpoint.""")
+tf.app.flags.DEFINE_string(
+    'input_path', './data/sample.png',
+    """Input image or video to be detected. Can process glob input such as """
+    """./data/00000*.png.""")
+tf.app.flags.DEFINE_string(
+    'out_dir', './data/out/', """Directory to dump output image or video.""")
+
+
+def video_demo():
+  """Detect videos."""
+
+  cap = cv2.VideoCapture(FLAGS.input_path)
+
+  # Define the codec and create VideoWriter object
+  # fourcc = cv2.cv.CV_FOURCC(*'XVID')
+  # fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
+  # fourcc = cv2.cv.CV_FOURCC('F', 'M', 'P', '4')
+  # out = cv2.VideoWriter('output.avi',fourcc, 30.0, (375,1242), True)
+  # out = VideoWriter('output.avi', frameSize=(1242, 375))
+  # out.open()
+
+  with tf.Graph().as_default():
+    # Load model
+    mc = kitti_squeezeDet_config()
+    mc.BATCH_SIZE = 1
+    # model parameters will be restored from checkpoint
+    mc.LOAD_PRETRAINED_MODEL = False
+    model = SqueezeDet(mc, FLAGS.gpu)
+
+    saver = tf.train.Saver(model.model_params)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    assert ckpt and ckpt.model_checkpoint_path, 'Checkpoint is not found.'
+    ckpt_path = ckpt.model_checkpoint_path
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+      saver.restore(sess, ckpt_path)
+
+      times = {}
+      count = 0
+      while cap.isOpened():
+        t_start = time.time()
+        count += 1
+        out_im_name = os.path.joint(FLAGS.out_dir, str(count).zfill(6)+'.jpg')
+
+        # Load images from video and crop
+        ret, frame = cap.read()
+        if ret==True:
+          # crop frames
+          frame = frame[500:-205, 239:-439, :]
+          im_input = frame.astype(np.float32) - mc.BGR_MEANS
+        else:
+          break
+
+        t_reshape = time.time()
+        times['reshape']= t_reshape - t_start
+
+        # Detect 
+        det_boxes, det_probs, det_class = sess.run(
+            [model.det_boxes, model.det_probs, model.det_class],
+            feed_dict={model.image_input:[im_input], model.keep_prob: 1.0})
+
+        t_detect = time.time()
+        times['detect']= t_detect - t_reshape
+
+        # Filter
+        final_boxes, final_probs, final_class = model.filter_prediction(
+            det_boxes[0], det_probs[0], det_class[0])
+
+        keep_idx    = [idx for idx in range(len(final_probs)) \
+                          if final_probs[idx] > mc.PLOT_PROB_THRESH]
+        final_boxes = [final_boxes[idx] for idx in keep_idx]
+        final_probs = [final_probs[idx] for idx in keep_idx]
+        final_class = [final_class[idx] for idx in keep_idx]
+
+        t_filter = time.time()
+        times['filter']= t_filter - t_detect
+
+        # Draw boxes
+        
+        _draw_box(
+            frame, final_boxes,
+            [mc.CLASS_NAMES[idx]+': (%.2f)'% prob \
+                for idx, prob in zip(final_class, final_probs)],
+            (0, 0, 255)
+        )
+
+        t_draw = time.time()
+        times['draw']= t_draw - t_filter
+
+        # out.write(frame)
+        cv2.imwrite(out_im_name, frame)
+
+        times['total']= time.time() - t_start
+
+        # time_str = ''
+        # for t in times:
+        #   time_str += '{} time: {:.4f} '.format(t[0], t[1])
+        # time_str += '\n'
+        time_str = 'Total time: {:.4f}, detection time: {:.4f}, filter time: '\
+                   '{:.4f}'. \
+            format(times['total'], times['detect'], times['filter'])
+
+        print (time_str)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+  # Release everything if job is finished
+  cap.release()
+  # out.release()
+  cv2.destroyAllWindows()
+
+
+def image_demo():
+  """Detect image."""
+
+  with tf.Graph().as_default():
+    # Load model
+    mc = kitti_squeezeDet_config()
+    mc.BATCH_SIZE = 1
+    # model parameters will be restored from checkpoint
+    mc.LOAD_PRETRAINED_MODEL = False
+    model = SqueezeDet(mc, FLAGS.gpu)
+
+    saver = tf.train.Saver(model.model_params)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.checkpoint_dir)
+    assert ckpt and ckpt.model_checkpoint_path, 'Checkpoint is not found.'
+    ckpt_path = ckpt.model_checkpoint_path
+
+    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+      saver.restore(sess, ckpt_path)
+
+      for f in glob.iglob(FLAGS.input_path):
+        im = cv2.imread(f)
+        im = im.astype(np.float32, copy=False)
+        im = cv2.resize(im, (mc.IMAGE_WIDTH, mc.IMAGE_HEIGHT))
+        input_image = im - mc.BGR_MEANS
+
+        # Detect 
+        det_boxes, det_probs, det_class = sess.run(
+            [model.det_boxes, model.det_probs, model.det_class],
+            feed_dict={model.image_input:[input_image], model.keep_prob: 1.0})
+
+        # Filter
+        final_boxes, final_probs, final_class = model.filter_prediction(
+            det_boxes[0], det_probs[0], det_class[0])
+
+        keep_idx    = [idx for idx in range(len(final_probs)) \
+                          if final_probs[idx] > mc.PLOT_PROB_THRESH]
+        final_boxes = [final_boxes[idx] for idx in keep_idx]
+        final_probs = [final_probs[idx] for idx in keep_idx]
+        final_class = [final_class[idx] for idx in keep_idx]
+
+        # Draw boxes
+        _draw_box(
+            im, final_boxes,
+            [mc.CLASS_NAMES[idx]+': (%.2f)'% prob \
+                for idx, prob in zip(final_class, final_probs)],
+            (0, 0, 255)
+        )
+
+        file_name = os.path.split(f)[1]
+        out_file_name = os.path.join(FLAGS.out_dir, 'out_'+file_name)
+        cv2.imwrite(out_file_name, im)
+        print ('Image detection output saved to {}'.format(out_file_name))
+
+
+def main(argv=None):
+  if not tf.gfile.Exists(FLAGS.out_dir):
+    tf.gfile.MakeDirs(FLAGS.out_dir)
+  if FLAGS.mode == 'image':
+    image_demo()
+  else:
+    video_demo()
+
+if __name__ == '__main__':
+    tf.app.run()
